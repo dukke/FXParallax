6408532c4747b549f1671cdec28a1c5585469471
Pedro Duque Vieira <pedro.duquevieira@pixelduke.com>
2018-05-07 22:02:58 +0100

    Throw UnsupportedOperationException when calling getImage on ParallaxPane

--- src/main/java/com/pixelduke/control/ParallaxPane.java
+++ src/main/java/com/pixelduke/control/ParallaxPane.java
@@ -63,10 +63,14 @@ public class ParallaxPane extends Control {
         setContent(imageView);
     }
 
-    /* I just have this method because we need to have a method with this name to be able to have an  image property in FXML
-       not intended to be used. */
+    /*
+     * @treatAsPrivate I just have this method because we need to have a method with this name to be able to have an  image property in FXML.
+     * Not intended to be used.
+     * @deprecated Not intended to be used.
+     */
+    @Deprecated
     public final Image getImage() {
-        return null;
+        throw new UnsupportedOperationException("This method is not supported. It only exists so that FXML loader accepts the image property for this control");
     }
 
     /***************************************************************************
